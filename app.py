9e1f6fc224c147d8f31c920a3a32fd1b355525f6
Abitha Valli <abitha@192.168.1.11>
2024-11-14 15:22:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+# app.py
+from flask import Flask, render_template, jsonify
+import random
+from dotenv import load_dotenv
+load_dotenv()  # Automatically looks for a .env file in the current directory or parent directories
+
+
+app = Flask(__name__)
+
+# List of gifts
+gifts = [
+    {"image": "static/img/pouch.jpg", "description": "🎁 A Roll-up pencil case!"},
+    {"image": "static/img/throw.jpg", "description": "🎁 A Throw!"}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_gift', methods=['GET'])
+def get_gift():
+    selected_gift = random.choice(gifts)
+    return jsonify(selected_gift)
+
+if __name__ == '__main__':
+    app.run()
